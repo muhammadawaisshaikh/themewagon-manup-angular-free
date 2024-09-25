ae547811515de939917999f1b944604bd4b52d14
Muhammad Awais <muhammad.awais@royalcyber.com>
2024-09-25 18:52:53 +0500

    header dynamic using binding from an object

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,29 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,RouterLinkActive, NgFor],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  navItems = [
+    { name: 'Home', route: '/' },
+    { name: 'About', route: '/about' },
+    {
+      name: 'Speakers',
+      children: [
+        { name: 'Jayden', route: '/speakers/jayden' },
+        { name: 'Sara', route: '/speakers/sara' },
+        { name: 'Emma', route: '/speakers/emma' },
+        { name: 'Harriet', route: '/speakers/harriet' }
+      ]
+    },
+    { name: 'Schedule', route: '/schedule' },
+    { name: 'Blog', route: '/blog' },
+    { name: 'Contacts', route: '/contact' }
+  ];
 }
